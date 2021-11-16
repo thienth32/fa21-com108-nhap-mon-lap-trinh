170f051f3aadb49c131b147999ab4245e67f9797
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-16 15:47:07 +0700

    Update main.c

--- cp17314-ucln/main.c
+++ cp17314-ucln/main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int x, y, min, max, max_chia_2, ucln = 1;
+    int x, y, max, max_chia_2, ucln = 1;
     printf("Nhap so x:");
     scanf("%d", &x);
     printf("Nhap so y:");
     scanf("%d", &y);
-    min = x < y ? x : y;
     max = x > y ? x : y;
     max_chia_2 = (int) max/2;
     for (int i = 2; i <= max_chia_2; i++) {
-        if(min % i == 0 && max % i == 0){
+        if(x % i == 0 && y % i == 0){
             ucln = i;
         }
     }
