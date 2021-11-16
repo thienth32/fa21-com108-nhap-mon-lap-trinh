04dca378a0dc6761c418349d2b1b3ebdf4f9222d
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-16 16:02:33 +0700

    bcnn

---
+++ cp17314-bcnn/cmake-build-debug/CMakeFiles/cp17314_bcnn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cp17314_bcnn.dir/main.c.o"
+  "cp17314_bcnn"
+  "cp17314_bcnn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cp17314_bcnn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cp17314-bcnn/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, max, bcln;
+    // 1. Nhap x va y
+    printf("Nhap so x:");
+    scanf("%d", &x);
+    printf("Nhap so y:");
+    scanf("%d", &y);
+    // 2. tim so lon nhat trong 2 so x, y
+    max = x > y ? x : y;
+    // 3. tim boi chung (chac chan se chia het cho x va y)
+    bcln = x * y;
+    // 4. tao bien luu tru bcnn
+    int bcnn = bcln;
+    // 5. chay vong lap tu max den bcln
+    for (int i = max; i <= bcln ; i++) {
+        // 6. kiem tra xem so dang duoc duyet co chia het cho ca x & y hay khong
+        if(i % x == 0 && i % y == 0){
+            bcnn = i;
+            // 7. thoat khoi vong lap
+            break;
+        }
+    }
+
+    printf("bcnn của %d va %d = %d", x, y, bcnn);
+    return 0;
+}
