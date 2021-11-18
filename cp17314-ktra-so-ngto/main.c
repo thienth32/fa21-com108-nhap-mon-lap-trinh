7cdcb235a8aa02cc6087fef6aa566acbb734f651
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-18 15:34:58 +0700

    Update main.c

--- cp17314-ktra-so-ngto/main.c
+++ cp17314-ktra-so-ngto/main.c
@@ -19,23 +19,50 @@ bool ktr_so_ngto(x){
         return true;
     }
     return false;
-//    return count == 0;
-//    return true|false
 }
+void bai1(){
+    printf("moi ban nhap so:");
+    int x;
+    scanf("%d", &x);
+    if(ktr_so_ngto(x) == true){
+        printf("%d la so nguyen to\n", x);
+    }else{
+        printf("%d khong la so nguyen to\n", x);
+    }
+}
+void bai2(){
 
+}
+void bai3(){
+
+}
 int main() {
     int lua_chon;
+    int bai_toan;
     do {
-        printf("moi ban nhap so:");
-        int x;
-        scanf("%d", &x);
-        if(ktr_so_ngto(x) == true){
-            printf("%d la so nguyen to\n", x);
-        }else{
-            printf("%d khong la so nguyen to\n", x);
+        printf("1. Tim so nguyen to\n");
+        printf("2. Tim so chinh phuong\n");
+        printf("3. Tim so lon nhat trong 3 so a, b, c\n");
+        printf("Moi ban chon:");
+        scanf("%d", &bai_toan);
+        switch (bai_toan) {
+            case 1:
+                bai1();
+                break;
+            case 2:
+                bai2();
+                break;
+            case 3:
+                bai3();
+                break;
+            default:
+                printf("lua chon khong hop le!\n");
+                break;
         }
+
+
         printf("choi tiep chu?(0|1)");
         scanf("%d", &lua_chon);
-    } while (lua_chon == true);
+    } while (lua_chon == 1);
     return 0;
 }
