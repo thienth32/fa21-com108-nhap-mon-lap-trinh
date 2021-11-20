f350dd936ac9935d078be50909ab2ae0b0980950
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-11-20 16:05:59 +0700

    tính dung lương từ gb => đơn vị nhỏ hơn

--- cp17314-ham-fgets/main.c
+++ cp17314-ham-fgets/main.c
@@ -5,18 +5,48 @@ int tinh_so_lon_nhat(a, b, c){
     max = max > c ? max : c;
     return max;
 }
+
+int tinh_dung_luong(gb, type){
+    int ketqua;
+    switch (type) {
+        case 1:
+            ketqua = gb*1024*1024*1024;
+            break;
+        case 2:
+            ketqua = gb*1024*1024;
+            break;
+        case 3:
+            ketqua = gb*1024;
+            break;
+    }
+    return ketqua;
+}
 int main() {
 //    printf("Nhap ho va ten:");
 //    char name[100];
 //    fgets(name, 100, stdin);
 //    printf("%s", name);
-    int a, b, c;
-    printf("Nhap so a:");
-    scanf("%d", &a);
-    printf("Nhap so b:");
-    scanf("%d", &b);
-    printf("Nhap so c:");
-    scanf("%d", &c);
-    printf("So lon nhat trong 3 so la: %d", tinh_so_lon_nhat(a, b, c));
+    /**
+     * Bài 1 tính số lớn nhất trong 3 số
+     */
+//    int a, b, c;
+//    printf("Nhap so a:");
+//    scanf("%d", &a);
+//    printf("Nhap so b:");
+//    scanf("%d", &b);
+//    printf("Nhap so c:");
+//    scanf("%d", &c);
+//    printf("So lon nhat trong 3 so la: %d", tinh_so_lon_nhat(a, b, c));
+    /**
+     * Bài 2: quy đổi ra đơn vị dung lượng
+     */
+    printf("Nhap dung lương (gb):");
+    int gb, donvi;
+    int dungluong;
+    scanf("%d", &gb);
+    printf("Don vi quy doi (1-byte | 2-kilo byte | 3-mega byte):");
+    scanf("%d", &donvi);
+    dungluong = tinh_dung_luong(gb, donvi);
+    printf("Ket qua quy doi: %d", dungluong);
     return 0;
 }
